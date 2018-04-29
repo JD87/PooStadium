d28d40975c4cc21991f135b05a3568397d6c2796
david <david_jdcc@hotmail.com>
2018-04-29 11:15:11 -0600

    from cooler to test git. life counter, animations for slashing, getting hurt and dead, name tag and splash screen with silverhawk symbol

---
+++ app/src/main/java/com/example/david/poostadium/SplashActivity.java
@@ -0,0 +1,20 @@
+package com.example.david.poostadium;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Nomad on 24/05/2017.
+ */
+public class SplashActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+        finish();
+    }
+
+}
