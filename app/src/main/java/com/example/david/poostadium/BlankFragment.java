d20e0ea203878cbd3af27df2169ade6f377780fd
david <david_jdcc@hotmail.com>
2018-05-03 23:41:12 -0600

    fragment added, not functional yet

--- app/src/main/java/com/example/david/poostadium/BlankFragment.java
+++ app/src/main/java/com/example/david/poostadium/BlankFragment.java
@@ -1,23 +1,38 @@
 package com.example.david.poostadium;
 
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
+import android.text.format.Formatter;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import static android.content.Context.WIFI_SERVICE;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class BlankFragment extends Fragment {
+public class BlankFragment extends DialogFragment {
 
 EditText etx_socket, etx_ipadd;
+TextView txv_frag;
 Button btn_connect;
 
 
@@ -30,6 +45,7 @@ Button btn_connect;
         Intent i = new Intent(getActivity().getBaseContext(), WifiActivity.class);
         i.putExtra("frag_ipadd", etx_ipadd.getText().toString());
         i.putExtra("frag_socket", etx_socket.getText().toString());
+        startActivity(i);
     }
 
     @Override
@@ -41,11 +57,17 @@ Button btn_connect;
         etx_ipadd = view.findViewById(R.id.etx_ipadd);
         etx_socket = view.findViewById(R.id.etx_socket);
         btn_connect = view.findViewById(R.id.btn_connect);
+        txv_frag = view.findViewById(R.id.txv_frag);
+
+        WifiManager wifiManager = (WifiManager) getActivity().getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        String ipAddress = Formatter.formatIpAddress(wifiManager.getConnectionInfo().getIpAddress());
+
+        txv_frag.setText(ipAddress);
 
         btn_connect.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getContext(), "yup, btn down",Toast.LENGTH_LONG).show();
+                senddata();
             }
         });
 
--- app/src/main/java/com/example/david/poostadium/WifiActivity.java
+++ app/src/main/java/com/example/david/poostadium/WifiActivity.java
@@ -2,7 +2,11 @@ package com.example.david.poostadium;
 
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
+import android.app.Fragment;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -11,6 +15,14 @@ import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class WifiActivity extends AppCompatActivity {
 
@@ -18,10 +30,20 @@ public class WifiActivity extends AppCompatActivity {
     TextView txv_namep1, txv_namep2,txv_lifep1, txv_lifep2;
     ImageView imv_p1, imv_p2, imv_slash, imv_potion;
 
+    Fragment b_frag;
+
     float x_dwn = 0, x_up = 0, y_dwn = 0, y_up = 0;
+
+    String ipadd, socket, p2response = "";
+
+    Boolean frag_done = true;
+
+    Socket host_socket;
+    DataOutputStream dOut;
 
     MainCharacter character1 = new MainCharacter();
     MainCharacter character2 = new MainCharacter();
+    BlankFragment myFragment = new BlankFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,7 +65,11 @@ public class WifiActivity extends AppCompatActivity {
         txv_namep1.setText(character1.getName());
         txv_namep2.setText(character1.getName());
 
+        openFragment();
+
     }
+
+
 
     @Override
     public boolean onTouchEvent (MotionEvent event) {
@@ -62,31 +88,16 @@ public class WifiActivity extends AppCompatActivity {
                     if(x_up > x_dwn){
                         imv_slash.setRotationY(0);
                         fade_animation(imv_slash,250);
-                        ouch_animation(imv_p2,100,20);
-                        character1.attack(character2);
-                        if(character2.life <= 0){
-                            character2.setLife(0);
-                            imv_p2.animate().rotationY(1080).setDuration(2000);
-                        }
-                        txv_lifep2.setText(character2.getLife() + "");
+                        //send_wifi("hit");
                     }
                     else {
-                        imv_slash.setRotationY(180);
-                        fade_animation(imv_slash,250);
-                        ouch_animation(imv_p1,100,-20);
-                        character2.attack(character1);
-                        if(character1.life <= 0){
-                            character1.setLife(0);
-                            imv_p1.animate().rotationY(1080).setDuration(2000);
-                        }
-                        txv_lifep1.setText(character1.getLife() + "");
+                        //  Used to be get hit, but now ¯\_(ツ)_/¯
                     }
                 if (dy>150){
                     if(y_up > y_dwn) {
                         Log.i("fingering tag", "downers");
                     }
                     else {
-                        openFragment();
                         Log.i("fingering tag", "uppers");
                     }
                     // TODO
@@ -94,14 +105,134 @@ public class WifiActivity extends AppCompatActivity {
                 break;
         }
 
+
+        MyClientTask myClientTask = new MyClientTask(ipadd,Integer.parseInt(socket));
+        myClientTask.execute();
+
+        Log.i("tag wifi", p2response);
+        if(p2response.equals("hit")){
+            character1.attack(character2);
+            if(character2.life <= 0){
+                character2.setLife(0);
+                imv_p2.animate().rotationY(1080).setDuration(2000);
+            }
+            txv_lifep2.setText(character2.getLife() + "");
+        }
+
         return true;
     }
 
+    @Override
+    protected void onResume(){
+        super.onResume();
+
+        receiveFragData();
+        Log.i("tag  receivedfrag","onresume");
+
+    }
+
     private void openFragment(){
-        //PASS OVER THE BUNDLE TO OUR FRAGMENT
-        BlankFragment myFragment = new BlankFragment();
-        //THEN NOW SHOW OUR FRAGMENT
-        getSupportFragmentManager().beginTransaction().replace(R.id.frm_layout,myFragment).commit();
+
+        getSupportFragmentManager().beginTransaction().add(R.id.frm_layout,myFragment).commit();
+
+    }
+
+    private void receiveFragData(){
+        Intent i = getIntent();
+        ipadd = i.getStringExtra("frag_ipadd");
+        socket = i.getStringExtra("frag_socket");
+
+        if(ipadd == null)
+            ipadd = "localhost";
+
+        if(socket == null)
+            socket = String.valueOf(6789);
+        else
+            getSupportFragmentManager().beginTransaction().remove(myFragment).commit();
+
+        Log.i("tag receivedfrag ","ip: " + ipadd);
+        Log.i("tag receivedfrag ","socket: " + socket);
+
+
+    }
+
+    public void send_wifi(String message){
+
+        try {
+            host_socket = new Socket(ipadd, Integer.parseInt(socket));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            dOut = new DataOutputStream(host_socket.getOutputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            dOut.writeByte(1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            dOut.writeUTF(message);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            dOut.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public class MyClientTask extends AsyncTask<Void, Void, Void> {
+
+        String dstAddress;
+        int dstPort;
+        String response;
+
+        MyClientTask(String addr, int port){
+            dstAddress = addr;
+            dstPort = port;
+        }
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+
+            try {
+                Socket socket = new Socket(dstAddress, dstPort);
+                InputStream inputStream = socket.getInputStream();
+                ByteArrayOutputStream byteArrayOutputStream =
+                        new ByteArrayOutputStream(1024);
+                byte[] buffer = new byte[1024];
+
+                int bytesRead;
+                while ((bytesRead = inputStream.read(buffer)) != -1){
+                    byteArrayOutputStream.write(buffer, 0, bytesRead);
+                }
+
+                socket.close();
+                response = byteArrayOutputStream.toString("UTF-8");
+
+            } catch (UnknownHostException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            p2response = response + "";
+            super.onPostExecute(result);
+        }
+
     }
 
     public void fade_animation(final ImageView imView, int duration_ms){
